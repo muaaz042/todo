09cbebc50849ac20946f341be4cbb53b87f288f1
Muhammad Saad Hasan <saadhassan817@gmail.com>
2024-06-28 02:15:15 +0500

    todo app

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,6 +1,6 @@
 // comment the local URL and uncomment the production one before pushing to github, and vice versa while working locally.
 
-// const BACKEND_URL = "https://todo-backend-eight-neon.vercel.app";
-const BACKEND_URL = "http://localhost:5174";
+const BACKEND_URL = "https://todo-app-mern-xi.vercel.app";
+// const BACKEND_URL = "http://localhost:5174";
 
 export { BACKEND_URL };
