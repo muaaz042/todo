c917e8270d71ff9054cd19278cdcfaecb0137eea
Muhammad Saad Hasan <saadhassan817@gmail.com>
2024-06-28 02:51:20 +0500

    todo app

--- backend/Routes/UserRoutes.js
+++ backend/Routes/UserRoutes.js
@@ -1,97 +1,91 @@
-const router = require('express').Router();
-const bcypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const userSchema = require('../Models/UserModel.js');
-const { requireLogin } = require('../middleware/auth.js');
+const router = require("express").Router();
+const bcypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const userSchema = require("../Models/UserModel.js");
+const { requireLogin } = require("../Middleware/auth.js");
 
-
-router.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
-    try {
-      let user = await userSchema.findOne({ email });
-      if (user) {
-        return res.status(400).json({ error: 'User Already Exist' });
-      }
-  
-      const hashPass = await bcypt.hash(password, 10);
-      user = new userSchema({
-        name,
-        email,
-        password: hashPass,
-        plainTextPassword: password
-      });
-      await user.save();
-      return res.status(201).json({ message: 'User registered Successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await userSchema.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: "User Already Exist" });
     }
-  });
-  
 
-router.post('/login', async (req, res) => {
-    const {email, password } = req.body;
-    try {
-        let user = await userSchema.findOne({email});
-        if(!user){
-            return res.status(400).json({error: 'User does not Exist'})
-        }        
-        const isCompare = await bcypt.compare(password, user.password);
-        if(!isCompare){
-            return res.status(400).json({error: 'Invalid Credentials'})
-        }
-        const token = jwt.sign({_id: user._id}, "notes", {expiresIn: '1h'});
-        return res.json({token});
-
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
+    const hashPass = await bcypt.hash(password, 10);
+    user = new userSchema({
+      name,
+      email,
+      password: hashPass,
+      plainTextPassword: password,
+    });
+    await user.save();
+    return res.status(201).json({ message: "User registered Successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await userSchema.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "User does not Exist" });
+    }
+    const isCompare = await bcypt.compare(password, user.password);
+    if (!isCompare) {
+      return res.status(400).json({ error: "Invalid Credentials" });
+    }
+    const token = jwt.sign({ _id: user._id }, "notes", { expiresIn: "1h" });
+    return res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 router.put("/updateUser", requireLogin, async (req, res) => {
-    const id = req.user._id;
+  const id = req.user._id;
 
-    const { name, email, plainTextPassword } = req.body;
+  const { name, email, plainTextPassword } = req.body;
 
-    let data;
-    const hashPass = await bcypt.hash(plainTextPassword, 10);
+  let data;
+  const hashPass = await bcypt.hash(plainTextPassword, 10);
 
-    try {
-      data = await userSchema.findByIdAndUpdate(id, {
-        name,
-        email,
-        password:hashPass,
-        plainTextPassword
-      });
+  try {
+    data = await userSchema.findByIdAndUpdate(id, {
+      name,
+      email,
+      password: hashPass,
+      plainTextPassword,
+    });
 
-      await data.save().then(() => {
-        res.status(200).json({ message: "update successful" });
-      });
+    await data.save().then(() => {
+      res.status(200).json({ message: "update successful" });
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
+router.get("/", requireLogin, async (req, res) => {
+  try {
+    const user = await userSchema
+      .findById(req.user._id)
+      .select("-password -plainTextPassword");
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-
-router.get('/', requireLogin, async (req, res) =>{
-    try {
-        const user = await userSchema.findById(req.user._id).select("-password -plainTextPassword");
-        res.json(user);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-})
-
-router.get('/getUser', requireLogin, async (req, res) =>{
-    try {
-        const user = await userSchema.findById(req.user._id).select("-password");
-        res.json(user);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
-})
-
-
+router.get("/getUser", requireLogin, async (req, res) => {
+  try {
+    const user = await userSchema.findById(req.user._id).select("-password");
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = router;
--- backend/Routes/notesRoutes.js
+++ backend/Routes/notesRoutes.js
@@ -1,69 +1,67 @@
-const router = require('express').Router();
-const notesSchema = require('../Models/notesModel.js');
-const { requireLogin } = require('../middleware/auth.js');
+const router = require("express").Router();
+const notesSchema = require("../Models/notesModel.js");
+const { requireLogin } = require("../Middleware/auth.js");
 
-router.post('/addNote', requireLogin, async (req, res) => {
-    const { title, description } = req.body;
-    const userid = req.user._id;
-    try {
-        let note = new notesSchema({
-            userid,
-            title,
-            description,
-        });
-        await note.save();
-        return res.status(201).json({ message: 'Note Added Successfully' });
-    } catch (error) {
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-router.get('/getNotes', requireLogin, async (req, res) => {
+router.post("/addNote", requireLogin, async (req, res) => {
+  const { title, description } = req.body;
+  const userid = req.user._id;
   try {
-      let note = await notesSchema.find({ userid: req.user._id });
-      if(!note){
-          return res.status(400).json({ error: 'Do not have any note.' });
-      } 
-
-      return res.json({ note });
+    let note = new notesSchema({
+      userid,
+      title,
+      description,
+    });
+    await note.save();
+    return res.status(201).json({ message: "Note Added Successfully" });
   } catch (error) {
-      console.log(error.message);
-      return res.status(500).json({ error: 'Server error' });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+router.get("/getNotes", requireLogin, async (req, res) => {
+  try {
+    let note = await notesSchema.find({ userid: req.user._id });
+    if (!note) {
+      return res.status(400).json({ error: "Do not have any note." });
+    }
+
+    return res.json({ note });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "Server error" });
+  }
+});
 
 router.put("/updateNote/:id", requireLogin, async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const { title, description } = req.body;
+  const { title, description } = req.body;
 
-    let data;
+  let data;
 
-    try {
-      data = await notesSchema.findByIdAndUpdate(id, {
-        title,
-        description
-      });
+  try {
+    data = await notesSchema.findByIdAndUpdate(id, {
+      title,
+      description,
+    });
 
-      await data.save().then(() => {
-        res.status(200).json({ message: "update successful" });
-      });
-
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+    await data.save().then(() => {
+      res.status(200).json({ message: "update successful" });
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 router.delete("/deleteNote/:id", requireLogin, async (req, res) => {
-    const id = req.params.id;
-    try {
-      await notesSchema.findByIdAndDelete(id).then(() => {
-    res.status(200).json({ message: "Deletion Success" });
-      });
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  const id = req.params.id;
+  try {
+    await notesSchema.findByIdAndDelete(id).then(() => {
+      res.status(200).json({ message: "Deletion Success" });
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
