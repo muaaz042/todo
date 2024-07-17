c9315262e5d7c7395a5290a20f059998588fa48b
Muaaz Ahmad <muaaz.ahmad042@gmail.com>
2024-07-18 00:36:25 +0500

    Adding Footer

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='w-full md:px-24 bg-white shadow-inner shadow-gray-400 px-7 md:py-7 py-5 flex justify-between md:flex-row flex-col items-center'>
+            <div>2024 © All Rights Reserved</div>
+            <div>Build by <a href="#" className='underline hover:text-blue-400'>Muaaz Ahmad</a></div>
+        </div>
+    )
+}
+
+export default Footer;
--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import data from '../../constants/data';
 import Card from "../Components/Card.jsx";
+import Footer from "../Components/Footer.jsx";
 
 const About = () => {
     const navigate = useNavigate();
@@ -14,17 +15,21 @@ const About = () => {
     }, [navigate]);
 
     return (
-        <div className="grid grid-cols-1 gap-32 py-28 lg:px-40 md:px-24 sm:px-12 px-5">
-            {data.map((d, i) => (
-                <Card
-                    key={i}
-                    span={d.span}
-                    title={d.title}
-                    description={d.description}
-                    icon={d.icon}
-                />
-            ))}
+        <div>
+            <div className="grid grid-cols-1 gap-32 py-28 lg:px-40 md:px-24 sm:px-12 px-5">
+                {data.map((d, i) => (
+                    <Card
+                        key={i}
+                        span={d.span}
+                        title={d.title}
+                        description={d.description}
+                        icon={d.icon}
+                    />
+                ))}
+            </div>
+            <Footer />
         </div>
+
     );
 };
 
